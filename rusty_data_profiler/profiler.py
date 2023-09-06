5b38de25a082611f61d7625aa9ac81a9f7a3c1dc
thorrester <steven.forrester@shipt.com>
2023-09-06 22:56:21 +0000

    parse dtype

--- rusty_data_profiler/profiler.py
+++ rusty_data_profiler/profiler.py
@@ -1,11 +1,16 @@
-from typing import List
+from typing import List, Union, Optional
 from numpy.typing import NDArray
+import polars as pl
+import pandas as pd
 
 from .rusty_data_profiler import parse_array, FeatureBin
 
 
 class Profiler:
-    def __init__(self, feature_names, num_bins):
+    def __init__(
+        self,
+        data: Union[pl.DataFrame, pd.DataFrame, NDArray],
+    ):
         """
         Class used to generate a data profile from a pandas dataframe,
         polars dataframe or numpy array.
@@ -16,8 +21,18 @@ class Profiler:
             num_bins:
                 Number of bins to use for the histogram.
         """
-        self.feature_names = feature_names
-        self.num_bins = num_bins
+
+        self.types = self._parse_types(data)
+
+    def _parse_types(self, data: Union[pl.DataFrame, pd.DataFrame, NDArray]) -> NDArray:
+        if isinstance(data, pl.DataFrame):
+            return data.schema
+        elif isinstance(data, pd.DataFrame):
+            return data.dtypes.apply(lambda x: x.name).to_dict()
+        elif isinstance(data, NDArray):
+            return {"dtype": data.dtype}
+        else:
+            raise ValueError(f"Unsupported data type: {type(data)}")
 
     def parse(self, array: NDArray) -> List[FeatureBin]:
         return parse_array(
